cf98de98d71b6011309402ca23f83f34657f5a3c
richardviana <richardpassostk@gmail.com>
2022-08-21 17:28:45 -0500

    adicionado cadastro e atutenticação de usuarios

--- app/src/main/java/com/richard/firebase/TelaPrincipal.java
+++ app/src/main/java/com/richard/firebase/TelaPrincipal.java
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.google.firebase.ktx.Firebase;
-
-public class MainActivity extends AppCompatActivity {
+public class TelaPrincipal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tela_principal);
+
+        getSupportActionBar().hide();
     }
 }
